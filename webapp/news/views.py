4dccfb92caa30a6007ac5ce9672a50a5f5509400
EternalGospod <kop.02@mail.ru>
2023-09-20 01:51:31 +0300

    commit

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from .models import  Articles
 # Create your views here.
 def news_home(request):
-    news = Articles.objects.all() # обращаемся к обьектам и получаем их все
+    news = Articles.objects.order_by('-date') # обращаемся к обьектам и получаем их все и сортируем по дате
     return render(request, 'news/news_home.html', {'news' : news}) #по ключу ньюс передаем обьект ньюс
+
+def create (request):
+    return render(request, 'news/create.html') # возвращает определенный шаблон
