863c704500d074975d3fbb26139c75a103f68153
EternalGospod <kop.02@mail.ru>
2023-09-27 02:13:51 +0300

    карта с полузнком

---
+++ webapp/map/static/map/js/zoom.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const slider = document.getElementById('slider');
+    const image = document.getElementById('image');
+
+    slider.addEventListener('input', () => {
+        const scale = slider.value;
+        image.style.transform = `scale(${scale})`;
+    });
+});
